e7c9cece4ef477b70dae292dc56540be07aec027
fra <francesco.zuppichini@gmail.com>
2022-03-16 14:35:33 +0100

    minor changes

--- src/transformers/models/maskformer/feature_extraction_maskformer.py
+++ src/transformers/models/maskformer/feature_extraction_maskformer.py
@@ -153,7 +153,7 @@ class MaskFormerFeatureExtractor(FeatureExtractionMixin, ImageFeatureExtractionM
             width = int(np.ceil(width / self.size_divisibility)) * self.size_divisibility
 
         size = (width, height)
-        image = self.resize(image, size=size)
+        image = self.resize(image, size=size, resample=self.resample)
 
         if target is not None:
             target = self.resize(target, size=size, resample=Image.NEAREST)
